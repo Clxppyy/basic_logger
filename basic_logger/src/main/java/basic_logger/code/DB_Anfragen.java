95bea8fdb526b9b72491fc380867d39d97e65412
Clxppy <134009803+Clxppyy@users.noreply.github.com>
2024-02-02 23:51:42 +0100

    hashing works, getData() problems

--- basic_logger/src/main/java/basic_logger/code/DB_Anfragen.java
+++ basic_logger/src/main/java/basic_logger/code/DB_Anfragen.java
@@ -3,73 +3,36 @@ package basic_logger.code;
 import java.sql.*;
 
 public final class DB_Anfragen extends DB_Verbinder {
-
-    String giveBack;
-
-    public boolean getUsername(String username) {
+    public boolean getData(String keyUsername, String keyPassword){
         try {
-            sqlAnfrage = "SELECT username FROM logger WHERE username = ?";
+            sqlAnfrage = "SELECT * FROM logger WHERE username = ? AND password = ?";
             PreparedStatement p = getConnectionToActualDB().prepareStatement(sqlAnfrage);
-            p.setString(1, username);
+            p.setString(1, keyUsername);
+            p.setString(2, keyPassword);
             ResultSet ergebnis = p.executeQuery(sqlAnfrage);
             ergebnis.next();
-            giveBack = ergebnis.getString("username");
             ergebnis.close();
             p.close();
             return true;
         } catch (SQLException ex) {
-            DB_Output.myDebug("Error in getUsername(): " + ex.getLocalizedMessage());
+            DB_Output.myDebug("Error in getData(): " + ex.getLocalizedMessage());
             return false;
         }
     }
 
-    public boolean getPassword(String password) {
+    public boolean insertData(String keyUsername, String keyPassword) {
         try {
-            sqlAnfrage = "SELECT password FROM logger WHERE password = ?";
-            PreparedStatement p = getConnectionToActualDB().prepareStatement(sqlAnfrage);
-            p.setString(1, password);
-            ResultSet ergebnis = p.executeQuery(sqlAnfrage);
-            ergebnis.next();
-            giveBack = ergebnis.getString("password");
-            ergebnis.close();
-            p.close();
-            return true;
-        } catch (SQLException ex) {
-            DB_Output.myDebug("Error in getPassword(): " + ex.getLocalizedMessage());
-            return false;
-        }
-    }
-
-    public boolean insertUsername(String username) {
-        try {
-            PreparedStatement p = getConnectionToActualDB().prepareStatement("INSERT INTO logger (username) VALUES (?)");
-
-            p.setString(1, username);
+            PreparedStatement p = getConnectionToActualDB().prepareStatement("INSERT INTO logger (username, password) VALUES (?,?)");
+            p.setString(1, keyUsername);
+            p.setString(2, keyPassword);
             p.addBatch();
             getConnectionToActualDB().setAutoCommit(false);
-            p.executeBatch(); // Daten an DB senden
+            p.executeBatch(); // send data to DB
             getConnectionToActualDB().setAutoCommit(true);
             p.close();
             return true;
         } catch (SQLException ex) {
-            DB_Output.myDebug("Error in insertUsername(): " + ex.getLocalizedMessage());
-            return false;
-        }
-    }
-
-    public boolean insertPassword(String password) {
-        try {
-            PreparedStatement p = getConnectionToActualDB().prepareStatement("INSERT INTO logger (password) VALUES (?)");
-
-            p.setString(1, password);
-            p.addBatch();
-            getConnectionToActualDB().setAutoCommit(false);
-            p.executeBatch(); // Daten an DB senden
-            getConnectionToActualDB().setAutoCommit(true);
-            p.close();
-            return true;
-        } catch (SQLException ex) {
-            DB_Output.myDebug("Error in insertPassword(): " + ex.getLocalizedMessage());
+            DB_Output.myDebug("Error in insertData(): " + ex.getLocalizedMessage());
             return false;
         }
     }
--- basic_logger/src/main/java/basic_logger/code/Main.java
+++ basic_logger/src/main/java/basic_logger/code/Main.java
@@ -5,6 +5,7 @@ import org.mindrot.jbcrypt.BCrypt;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -27,9 +28,9 @@ public abstract class Main implements ExecutorService {
     static DB_Verbinder gateOpener = new DB_Verbinder();
     static Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*CHECKPOINT, you have to type in your own directory of your own database --> */
-        if(gateOpener.db_open("C:\\Users\\Clxppy\\IdeaProjects\\basic_logger", "db_logger.db")){
+        if(gateOpener.db_open("C:\\Users\\Clxppy\\IdeaProjects\\basic_logger\\", "db_logger.db")){
             /*AVOIDABLE,  from here on now the logging process is of course your thing to do.*/
             System.out.println("'L' for login / 'C' for creating new profile / 'E' for exit");
             boolean lever = true;
@@ -60,7 +61,7 @@ public abstract class Main implements ExecutorService {
         System.exit(1);
     }
 
-    static void organize(Boolean lever){
+    static void organize(Boolean lever) throws InterruptedException {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         System.out.println("Enter a username:");
         AtomicReference<String> username = new AtomicReference<>(scan.nextLine());
@@ -68,24 +69,33 @@ public abstract class Main implements ExecutorService {
         System.out.println("Enter a password:");
         AtomicReference<String> password = new AtomicReference<>(scan.nextLine());
         executor.execute(() -> password.set(encryptInput(password.get())));
-        //lever = false means the user wants to log in
-        //lever = true means a new profile shall be created
-        ToDB(lever || !executor.isTerminated(), username.get(), password.get());
+        executor.shutdown();
+        while(executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)){
+            if(!lever){
+                //lever = false means the user wants to log in
+                ToDB(false, username.get(), password.get());
+                break;
+            }
+            else {
+                //lever = true means a new profile shall be created
+                ToDB(true, username.get(), password.get());
+                break;
+            }
+        }
     }
 
     static String encryptInput(String input){
         //Copyright (c) 2006 Damien Miller <djm@mindrot.org>
-        return BCrypt.hashpw(input, BCrypt.gensalt(15));
+        String hashed = BCrypt.hashpw(input, BCrypt.gensalt(15));
+        System.out.println(hashed);
+        return hashed;
     }
 
     static void ToDB(boolean lever, String key_username, String key_password) {
         DB_Anfragen opener = new DB_Anfragen();
-
-        //Maybe change to recursion
-        
         if(!lever){
             //lever = false means the user wants to log in
-            if(opener.getUsername(key_username) && opener.getPassword(key_password)){
+            if(opener.getData(key_username, key_password)){
                 System.out.println("Login successful!");
             }
             else {
@@ -95,7 +105,7 @@ public abstract class Main implements ExecutorService {
         }
         else {
             //lever = true means a new profile shall be created
-            if(opener.insertUsername(key_username) && opener.insertPassword(key_password)){
+            if(opener.insertData(key_username, key_password)){
                 System.out.println("Profile successfully created!");
             }
             else {
